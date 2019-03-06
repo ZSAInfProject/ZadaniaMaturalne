18030e46233200888ed3a5fd27569bba34f94ee9
Kajetan Pynka <siema1234@gazeta.pl>
2019-03-06 20:43:24 +0100

    Added exercise number 58

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,19 @@ def page():
 @app.route('/zadanie/58/', methods=['POST'])
 def load_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-    return "" + f.filename
+        f1 = request.files['file']
+        f2 = request.files['file2']
+        f3 = request.files['file3']
+        f1.save(secure_filename(f1.filename))
+        f2.save(secure_filename(f2.filename))
+        f3.save(secure_filename(f3.filename))
+        from zbior_zadan.zad58.zad58 import zadanie58
+        zadanie58()
+        file = open('systemy_wyniki.txt', 'r')
+        data = file.read()
+        file.close()
+        data = data.split('\n')
+    return render_template('zadanie58.html', data=data)
 
 @app.route('/zadanie/60/')
 def page2():
